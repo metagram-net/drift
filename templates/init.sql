a596ffd2430361930d0f5afedab07362120263c4
Jeremy Kaplan <jdkaplan@metagram.net>
2022-02-23 19:48:41 -0800

    Fix formatting in init.sql comment

--- templates/init.sql
+++ templates/init.sql
@@ -6,10 +6,10 @@ First, this includes a drift:no-transaction directive, which tells Drift to
 skip two steps it would normally take:
 1. Opening a transaction around the migration file. In Postgres, DDL can be
    done in a transaction. This can make some migrations safer, so Drift assumes
-transactions as the default.
+   transactions as the default.
 2. Calling _drift_claim_migration(id, slug) before running the file. Since this
    claim would fail on a duplicate ID, this ensures we never run a migration
-twice (since it's normally part of a transaction).
+   twice (since it's normally part of a transaction).
 
 It doesn't make sense to call _drift_claim_migration yet, because this is the
 migration that defines it!
